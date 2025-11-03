b7fb2c4054a809e7b8d5d6c4b3ec52638cf72890
Fedor Antonenko <cupofmilk80@gmail.com>
2025-11-03 16:52:03 +0300

    - Обновлена работа приложения для соответствия работы тестов (friends / Friend add)

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.controller.exception.FriendsAddingException;
 import ru.yandex.practicum.filmorate.controller.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.FriendshipStatus;
@@ -84,22 +83,19 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public void addFriend(Long userId, Long friendId) {
-        if (userId.equals(friendId)) {
-            throw new FriendsAddingException("Нельзя добавить себя в друзья");
-        }
+        String sql = "INSERT INTO user_friendships (user_id, friend_id, confirmed) VALUES (?, ?, FALSE)";
 
-        String checkReverseSql = "SELECT COUNT(*) FROM user_friendships WHERE user_id = ? AND friend_id = ?";
-        Integer reverseExists = jdbcTemplate.queryForObject(checkReverseSql, Integer.class, friendId, userId);
-
-        if (reverseExists != null && reverseExists > 0) {
-            String updateSql = "UPDATE user_friendships SET confirmed = TRUE WHERE (user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)";
-            jdbcTemplate.update(updateSql, userId, friendId, friendId, userId);
+        if (userHasFriend(userId, friendId)) {
             return;
         }
 
-        String insertSql = "INSERT INTO user_friendships (user_id, friend_id, confirmed) VALUES (?, ?, TRUE)";
-        jdbcTemplate.update(insertSql, userId, friendId);
-        jdbcTemplate.update(insertSql, friendId, userId);
+        jdbcTemplate.update(sql, userId, friendId);
+    }
+
+    private boolean userHasFriend(Long userId, Long friendId) {
+        String sql = "SELECT COUNT(*) FROM user_friendships WHERE user_id = ? AND friend_id = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, userId, friendId);
+        return count != null && count > 0;
     }
 
     @Override
